dc31b75eff394a6a361c95ac822518d37206dfa7
bossninja <gitmonkey@bossninja.com>
2012-02-23 22:18:35 -0600

    style changes

--- views/admin/import.php
+++ views/admin/import.php
@@ -1,4 +1,6 @@
-<section class="title"><h4>Import Facebook Galleries:&nbsp;&nbsp;</h4><h4><?php if($facebook->getUser()){?><img src="https://graph.facebook.com/<?=$facebook->getUser() ?>/picture" width="30" align="absmiddle"  /> <?=$fb_user_profile['name'] ?><?php }?></h4></section>
+﻿<section class="title">
+	<h4>Import Facebook Galleries:&nbsp;<img src="https://graph.facebook.com/<?=$facebook->getUser()?>/picture" width="30" align="absmiddle"  /> <?=$fb_user_profile['name'] ?><?php }?></h4>
+</section>
 <section class="item">
 	<table>
 	    <tr>
@@ -9,7 +11,7 @@
 	    <tr>
 	        <td class=""><?=$v['name'] ?></td>
 	        <td class="actions collapse"><form id="frm_<?=$v['id'] ?>" action="<?=site_url('admin/'.$this->module.'/import') ?>" method="post" ><button type="submit" name="btnAction" value="save" class="btn green">Import</button><input type="hidden" value="<?=$v['id'] ?>" name="aid" /></form></td>
-	    </tr>        
+	    </tr>
         <?php }?>        
      </table>
 </section>
