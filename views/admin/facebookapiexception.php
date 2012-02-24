80cef7f143df5273ac0f9a23bad0fcd4f6231ce7
bossninja <gitmonkey@bossninja.com>
2012-02-23 22:03:22 -0600

    added a facebook exceptions view for error reporting from facebook

---
+++ views/admin/facebookapiexception.php
@@ -0,0 +1,12 @@
+<section class="title"><h4>Facebook API Exception</h4></section>
+<section class="item">
+
+	<table class="fbexception">
+		<tr>
+			<td>
+				<?=$exception?>
+			</td>
+		</tr>
+	</table>
+
+</section>
